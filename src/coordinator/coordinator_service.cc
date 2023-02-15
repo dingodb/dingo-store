f2707bf2cd960484ed944af3201828b44758af37
Ketor <d.ketor@gmail.com>
2023-02-15 13:10:37 +0800

    [coordinator] Initial commit with only hello() support.
    Signed-off-by: Ketor <d.ketor@gmail.com>

---
+++ src/coordinator/coordinator_service.cc
@@ -0,0 +1,30 @@
+// Copyright (c) 2023 dingodb.com, Inc. All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "coordinator/coordinator_service.h"
+
+namespace dingodb {
+
+    void CoordinatorServiceImpl::Hello(google::protobuf::RpcController *controller,
+                                 const dingodb::coordinator::HelloRequest *request,
+                                 dingodb::coordinator::HelloResponse *response,
+                                 google::protobuf::Closure *done) {
+        brpc::ClosureGuard done_guard(done);
+        LOG(INFO) << "Hello request: " << request->hello();
+
+        response->set_status(static_cast<dingodb::coordinator::CoordinatorStatus>(0));
+        response->set_status_detail("OK");
+    }
+
+} // namespace dingodb
