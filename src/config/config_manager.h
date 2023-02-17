98bc680b9ed09ca372015be512a60f2d6ee8b0c9
Ketor <d.ketor@gmail.com>
2023-02-17 15:11:21 +0800

    [config] Fix compile error in compile_manager.
    Signed-off-by: Ketor <d.ketor@gmail.com>

--- src/config/config_manager.h
+++ src/config/config_manager.h
@@ -1,11 +1,11 @@
 // Copyright (c) 2023 dingodb.com, Inc. All Rights Reserved
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 #define DINGODB_CONFIG_MANAGER_H
 
 #include <map>
+#include <mutex>
 #include <shared_mutex>
 
 #include "glog/logging.h"
@@ -24,14 +25,15 @@
 #include "config/config.h"
 #include "config/yaml_config.h"
 
-template <typename T> struct DefaultSingletonTraits;
+template <typename T>
+struct DefaultSingletonTraits;
 
 namespace dingodb {
 
 // Manage all config
 class ConfigManager {
  public:
-  static ConfigManager* GetInstance();
+  static ConfigManager *GetInstance();
 
   bool IsExist(const std::string &name);
   void Register(const std::string &name, std::shared_ptr<Config> config);
@@ -44,11 +46,9 @@ class ConfigManager {
   DISALLOW_COPY_AND_ASSIGN(ConfigManager);
 
   std::shared_mutex mutex_;
-  std::map<std::string, std::shared_ptr<Config> > configs_; 
+  std::map<std::string, std::shared_ptr<Config> > configs_;
 };
 
-} // namespace dingodb 
+}  // namespace dingodb
 
-
-
-#endif // DINGODB_CONFIG_MANAGER_H
+#endif  // DINGODB_CONFIG_MANAGER_H
