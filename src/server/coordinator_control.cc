c82de66fd570d029d8b0a4c9c88bb90faa044c7f
Ketor <d.ketor@gmail.com>
2023-03-01 00:48:04 +0800

    [coordinator] Add StoreHeartbeat code to coordinator client.
    Signed-off-by: Ketor <d.ketor@gmail.com>

--- src/server/coordinator_control.cc
+++ src/server/coordinator_control.cc
@@ -41,25 +41,40 @@ uint64_t CoordinatorControl::CreatePartitionId() {
   return next_partition_id_++;
 }
 
+// TODO: data persistence
 uint64_t CoordinatorControl::UpdateStoreMap(const pb::common::Store& store) {
+  bool need_update_epoch = false;
   for (int i = 0; i < store_map_.stores_size(); i++) {
     auto* old_store = store_map_.mutable_stores(i);
     if (old_store->id() == store.id()) {
+      // update old store properties
       if (old_store->status() != store.status()) {
         LOG(INFO) << "STORE STATUS CHANGE store_id = " << store.id()
                   << " old status = " << old_store->status()
                   << " new status = " << store.status();
         store_map_.set_epoch(store_map_.epoch() + 1);
+        need_update_epoch = true;
       }
       old_store->CopyFrom(store);
       break;
     }
   }
 
+  if (!need_update_epoch) {
+    // create new store
+    auto* new_store = store_map_.add_stores();
+    new_store->CopyFrom(store);
+  }
+
+  LOG(INFO) << "UpdateStoreMap " << store_map_.DebugString();
+
   return store_map_.epoch();
 }
 
-uint64_t CoordinatorControl::UpdateRegionMap(const pb::common::Region& region) {
+bool CoordinatorControl::UpdateOneRegionMap(const pb::common::Region& region) {
+  bool need_to_add_region = true;
+  bool need_to_update_epoch = false;
+
   for (int i = 0; i < region_map_.regions_size(); i++) {
     auto* old_region = region_map_.mutable_regions(i);
     if (old_region->id() == region.id()) {
@@ -67,38 +82,50 @@ uint64_t CoordinatorControl::UpdateRegionMap(const pb::common::Region& region) {
         LOG(INFO) << "REGION STATUS CHANGE region_id = " << region.id()
                   << " old status = " << old_region->status()
                   << " new status = " << region.status();
-        region_map_.set_epoch(region_map_.epoch() + 1);
       }
       old_region->CopyFrom(region);
+      need_to_add_region = false;
+      need_to_update_epoch = true;
       break;
     }
+  }
+
+  if (need_to_add_region) {
+    // add new region to regionmap (may only used for testing, not exist in
+    // production env)
+    auto* new_region = region_map_.add_regions();
+    new_region->CopyFrom(region);
+    need_to_update_epoch = true;
+  }
+
+  return need_to_update_epoch;
+}
+
+// TODO: data persistence
+uint64_t CoordinatorControl::UpdateRegionMap(const pb::common::Region& region) {
+  if (UpdateOneRegionMap(region)) {
+    region_map_.set_epoch(region_map_.epoch() + 1);
   }
 
   return region_map_.epoch();
 }
 
+// TODO: data persistence
 uint64_t CoordinatorControl::UpdateRegionMapMulti(
     std::vector<pb::common::Region> regions) {
   bool need_to_update_epoch = false;
-  for (int i = 0; i < region_map_.regions_size(); i++) {
-    auto* old_region = region_map_.mutable_regions(i);
-    for (const auto& region : regions) {
-      if (old_region->id() == region.id()) {
-        if (old_region->status() != region.status()) {
-          LOG(INFO) << "REGION STATUS CHANGE region_id = " << region.id()
-                    << " old status = " << old_region->status()
-                    << " new status = " << region.status();
-        }
-        old_region->CopyFrom(region);
-        need_to_update_epoch = true;
-        break;
-      }
+
+  for (const auto& region : regions) {
+    if (UpdateOneRegionMap(region)) {
+      need_to_update_epoch = true;
     }
   }
 
   if (need_to_update_epoch) {
     region_map_.set_epoch(region_map_.epoch());
   }
+
+  LOG(INFO) << "UpdateRegionMapMulti " << region_map_.DebugString();
 
   return region_map_.epoch();
 }
