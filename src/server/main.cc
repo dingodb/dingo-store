75c72d87c398d76029ccbc876100c988e80ac70e
Huzx <huzongxing0826@gmail.com>
2023-03-10 13:25:50 +0800

    [server] rename variables about rpc server and dingo server

--- src/server/main.cc
+++ src/server/main.cc
@@ -69,31 +69,31 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  auto *dingodb_server = dingodb::Server::GetInstance();
-  dingodb_server->SetRole(role);
-  if (!dingodb_server->InitConfig(FLAGS_conf)) {
+  auto *dingo_server = dingodb::Server::GetInstance();
+  dingo_server->SetRole(role);
+  if (!dingo_server->InitConfig(FLAGS_conf)) {
     LOG(ERROR) << "InitConfig failed!";
     return -1;
   }
-  if (!dingodb_server->InitLog()) {
+  if (!dingo_server->InitLog()) {
     LOG(ERROR) << "InitLog failed!";
     return -1;
   }
-  if (!dingodb_server->InitServerID()) {
+  if (!dingo_server->InitServerID()) {
     LOG(ERROR) << "InitServerID failed!";
     return -1;
   }
 
   std::shared_ptr<dingodb::Config> const config = dingodb::ConfigManager::GetInstance()->GetConfig(role);
-  dingodb_server->SetServerEndpoint(GetServerEndPoint(config));
-  dingodb_server->SetRaftEndpoint(GetRaftEndPoint(config));
+  dingo_server->SetServerEndpoint(GetServerEndPoint(config));
+  dingo_server->SetRaftEndpoint(GetRaftEndPoint(config));
 
-  if (!dingodb_server->InitEngines()) {
+  if (!dingo_server->InitEngines()) {
     LOG(ERROR) << "InitEngines failed!";
     return -1;
   }
 
-  brpc::Server server;
+  brpc::Server brpc_server;
   dingodb::CoordinatorControl coordinator_control;
   dingodb::CoordinatorServiceImpl coordinator_service;
   dingodb::MetaServiceImpl meta_service;
@@ -107,28 +107,28 @@ int main(int argc, char *argv[]) {
     meta_service.SetControl(&coordinator_control);
 
     // add service to brpc
-    if (server.AddService(&coordinator_service, brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
+    if (brpc_server.AddService(&coordinator_service, brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
       LOG(ERROR) << "Fail to add coordinator service!";
       return -1;
     }
-    if (server.AddService(&meta_service, brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
+    if (brpc_server.AddService(&meta_service, brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
       LOG(ERROR) << "Fail to add meta service!";
       return -1;
     }
 
-    if (braft::add_service(&raft_server, dingodb_server->RaftEndpoint()) != 0) {
+    if (braft::add_service(&raft_server, dingo_server->RaftEndpoint()) != 0) {
       LOG(ERROR) << "Fail to add raft service!";
       return -1;
     }
-    if (raft_server.Start(dingodb_server->RaftEndpoint(), nullptr) != 0) {
+    if (raft_server.Start(dingo_server->RaftEndpoint(), nullptr) != 0) {
       LOG(ERROR) << "Fail to start raft server!";
       return -1;
     }
     LOG(INFO) << "Raft server is running on " << raft_server.listen_address();
 
     // start meta region
-    auto engine = dingodb_server->GetEngine(dingodb::pb::common::Engine::ENG_RAFT_STORE);
-    dingodb::pb::error::Errno status = dingodb_server->StartMetaRegion(config, engine);
+    auto engine = dingo_server->GetEngine(dingodb::pb::common::Engine::ENG_RAFT_STORE);
+    dingodb::pb::error::Errno status = dingo_server->StartMetaRegion(config, engine);
     if (status != dingodb::pb::error::Errno::OK) {
       LOG(INFO) << "Init RaftNode and StateMachine Failed:" << status;
       return -1;
@@ -137,56 +137,56 @@ int main(int argc, char *argv[]) {
     // TODO: load data from kv engine into maps
 
   } else if (is_store) {
-    if (!dingodb_server->InitCoordinatorInteraction()) {
+    if (!dingo_server->InitCoordinatorInteraction()) {
       LOG(ERROR) << "InitCoordinatorInteraction failed!";
       return -1;
     }
-    if (!dingodb_server->ValiateCoordinator()) {
+    if (!dingo_server->ValiateCoordinator()) {
       LOG(ERROR) << "ValiateCoordinator failed!";
       return -1;
     }
-    if (!dingodb_server->InitStorage()) {
+    if (!dingo_server->InitStorage()) {
       LOG(ERROR) << "InitStorage failed!";
       return -1;
     }
-    if (!dingodb_server->InitStoreMetaManager()) {
+    if (!dingo_server->InitStoreMetaManager()) {
       LOG(ERROR) << "InitStoreMetaManager failed!";
       return -1;
     }
-    if (!dingodb_server->InitCrontabManager()) {
+    if (!dingo_server->InitCrontabManager()) {
       LOG(ERROR) << "InitCrontabManager failed!";
       return -1;
     }
 
-    store_service.set_storage(dingodb_server->GetStorage());
-    if (server.AddService(&store_service, brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
+    store_service.set_storage(dingo_server->GetStorage());
+    if (brpc_server.AddService(&store_service, brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
       LOG(ERROR) << "Fail to add store service!";
       return -1;
     }
 
     // raft server
-    if (braft::add_service(&raft_server, dingodb_server->RaftEndpoint()) != 0) {
+    if (braft::add_service(&raft_server, dingo_server->RaftEndpoint()) != 0) {
       LOG(ERROR) << "Fail to add raft service!";
       return -1;
     }
-    if (raft_server.Start(dingodb_server->RaftEndpoint(), nullptr) != 0) {
+    if (raft_server.Start(dingo_server->RaftEndpoint(), nullptr) != 0) {
       LOG(ERROR) << "Fail to start raft server!";
       return -1;
     }
     LOG(INFO) << "Raft server is running on " << raft_server.listen_address();
   }
 
-  if (!dingodb_server->Recover()) {
+  if (!dingo_server->Recover()) {
     LOG(ERROR) << "Recover failed!";
     return -1;
   }
 
   // Start server after raft server started.
-  if (server.Start(dingodb_server->ServerEndpoint(), nullptr) != 0) {
+  if (brpc_server.Start(dingo_server->ServerEndpoint(), nullptr) != 0) {
     LOG(ERROR) << "Fail to start server!";
     return -1;
   }
-  LOG(INFO) << "Server is running on " << server.listen_address();
+  LOG(INFO) << "Server is running on " << brpc_server.listen_address();
 
   // Wait until 'CTRL-C' is pressed. then Stop() and Join() the service
   while (!brpc::IsAskedToQuit()) {
@@ -195,11 +195,11 @@ int main(int argc, char *argv[]) {
   LOG(INFO) << "Server is going to quit";
 
   raft_server.Stop(0);
-  server.Stop(0);
+  brpc_server.Stop(0);
   raft_server.Join();
-  server.Join();
+  brpc_server.Join();
 
-  dingodb_server->Destroy();
+  dingo_server->Destroy();
 
   return 0;
 }
