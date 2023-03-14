dc0d962d765d5aa5722cbb9e1d871228c7c31099
huzx <huzongxing0826@gmail.com>
2023-03-14 17:21:01 +0800

    [feat][sdk] Add submodule to integrated java example

--- java/dingo-sdk/src/main/java/io/dingodb/DingoClient.java
+++ java/dingo-sdk/src/main/java/io/dingodb/DingoClient.java
@@ -8,18 +8,9 @@ import io.dingodb.client.ContextForClient;
 import io.dingodb.client.Result;
 import io.dingodb.client.ServiceOperation;
 import io.dingodb.client.StoreOperationType;
-import io.dingodb.common.Common;
-import io.dingodb.sdk.common.partition.PartitionDetailDefinition;
-import io.dingodb.sdk.common.partition.PartitionRule;
-import io.dingodb.sdk.common.table.ColumnDefinition;
 import io.dingodb.sdk.common.table.Table;
-import io.dingodb.sdk.common.table.TableDefinition;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
-import static io.dingodb.common.Common.Engine.ENG_ROCKSDB;
 
 public class DingoClient {
 
@@ -46,7 +37,6 @@ public class DingoClient {
     }
 
     public boolean upsert(String tableName, List<Object[]> keys, List<Object[]> values) {
-
         Result result = serviceOperation.operation(
                 tableName,
                 StoreOperationType.PUT,
@@ -61,49 +51,6 @@ public class DingoClient {
                 StoreOperationType.GET,
                 ContextForClient.builder().keyList(keys).build()
         );
-
         return result;
-    }
-
-    public static void main(String[] args) {
-        DingoClient dingoClient = new DingoClient("192.168.1.201:22001", 10);
-        dingoClient.open();
-
-        String tableName = "test";
-
-        boolean test1 = dingoClient.dropTable(tableName);
-        System.out.println(test1);
-
-        ColumnDefinition c1 = new ColumnDefinition("id", "integer", "", 0, 0, false, 0, "");
-        ColumnDefinition c2 = new ColumnDefinition("name", "varchar", "", 0, 0, false, -1, "");
-
-        PartitionDetailDefinition detailDefinition = new PartitionDetailDefinition(null, null, Arrays.asList(new Object[]{1}));
-        PartitionRule partitionRule = new PartitionRule(null, null, Arrays.asList(detailDefinition));
-
-        TableDefinition tableDefinition = new TableDefinition(tableName,
-                Arrays.asList(c1, c2),
-                1,
-                0,
-                partitionRule,
-                Common.Engine.ENG_ROCKSDB.name(),
-                null);
-        boolean isSuccess = dingoClient.createTable(tableDefinition);
-
-        System.out.println(isSuccess);
-
-        try {
-            Thread.sleep(20000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        boolean test = dingoClient.upsert(tableName, Collections.singletonList(new Object[]{1, null}), Collections.singletonList(new Object[]{1, "zhangsan"}));
-        System.out.println(test);
-
-        Result result = dingoClient.get(tableName, Collections.singletonList(new Object[]{1, null}));
-
-        for (Object[] value : result.getValues()) {
-            System.out.println(Arrays.toString(value));
-        }
     }
 }
