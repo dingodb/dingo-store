e3c66dd4057031f25038fc1eaa6298f3cfc9a0f1
Ketor <d.ketor@gmail.com>
2023-04-04 01:49:16 +0800

    [feat][cmake] Add gperftools to cmake build.
    The gperftools is disabled by default, use -DLINK_TCMALLOC=ON to build
    with gperftools.
    
    Apache bRPC support gperftools more than jemalloc, with both tcmalloc
    and cpu,heap profiler.
    
    Signed-off-by: Ketor <d.ketor@gmail.com>

---
+++ cmake/gperftools.cmake
@@ -0,0 +1,42 @@
+# Copyright (c) 2023 dingodb.com, Inc. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+INCLUDE(ExternalProject)
+
+SET(GPERFTOOLS_SOURCES_DIR ${THIRD_PARTY_PATH}/gperftools)
+SET(GPERFTOOLS_BINARY_DIR ${THIRD_PARTY_PATH}/build/gperftools)
+SET(GPERFTOOLS_INSTALL_DIR ${THIRD_PARTY_PATH}/install/gperftools)
+SET(GPERFTOOLS_INCLUDE_DIR "${GPERFTOOLS_INSTALL_DIR}/include" CACHE PATH "gperftools include directory." FORCE)
+SET(GPERFTOOLS_LIBRARIES "${GPERFTOOLS_INSTALL_DIR}/lib/libtcmalloc_and_profiler.a" CACHE FILEPATH "gperftools library." FORCE)
+
+FILE(WRITE ${GPERFTOOLS_SOURCES_DIR}/src/copy_repo.sh
+        "mkdir -p ${GPERFTOOLS_SOURCES_DIR}/src/extern_gperftools/ && cp -rf ${CMAKE_SOURCE_DIR}/contrib/gperftools/* ${GPERFTOOLS_SOURCES_DIR}/src/extern_gperftools/")
+
+execute_process(COMMAND sh ${GPERFTOOLS_SOURCES_DIR}/src/copy_repo.sh)
+
+ExternalProject_Add(
+        extern_gperftools
+        ${EXTERNAL_PROJECT_LOG_ARGS}
+        SOURCE_DIR ${GPERFTOOLS_SOURCES_DIR}/src/extern_gperftools/
+        # BINARY_DIR ${GPERFTOOLS_BINARY_DIR}
+        PREFIX ${GPERFTOOLS_INSTALL_DIR}
+        BUILD_IN_SOURCE 1
+        CONFIGURE_COMMAND sh autogen.sh COMMAND sh ./configure --prefix=${GPERFTOOLS_INSTALL_DIR} --disable-debugalloc --enable-frame-pointers
+        BUILD_COMMAND $(MAKE)
+        INSTALL_COMMAND $(MAKE) install
+)
+
+ADD_LIBRARY(gperftools STATIC IMPORTED GLOBAL)
+SET_PROPERTY(TARGET gperftools PROPERTY IMPORTED_LOCATION ${GPERFTOOLS_LIBRARIES})
+ADD_DEPENDENCIES(gperftools extern_gperftools)
