238e227999df7823c889dc3a735f40b914d5c4e8
Huzx <huzongxing0826@gmail.com>
2023-04-12 19:16:34 +0800

    [feat][server] Add version and commit hash to server when compile

---
+++ src/common/version.cc
@@ -0,0 +1,15 @@
+
+
+#include "common/version.h"
+
+namespace dingodb {
+
+#ifndef GIT_VERSION
+#define GIT_VERSION "unknown"
+#endif
+
+#ifndef GIT_TAG_NAME
+#define GIT_TAG_NAME "v0.6.0"
+#endif
+
+}  // namespace dingodb
