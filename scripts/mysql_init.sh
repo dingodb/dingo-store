a7092381d9d12f3ed6832ab0e1ede41c6d3b14cc
gary <1625721671@qq.com>
2023-05-04 18:56:06 +0800

    [chore][deploy] Add mySql init

---
+++ scripts/mysql_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sleep 60
+
+# 运行 Java 程序
+java -cp dingo-mysql-init-0.6.0-SNAPSHOT.jar io.dingodb.mysql.MysqlInit $SERVER_HOST:$COORDINATOR_SERVER_START_PORT
+
+# 检查退出状态
+if [ $? -eq 0 ]
+then
+  echo "Java mysql 初始化成功"
+else
+  echo "Java mysql 初始化失败"
+fi
