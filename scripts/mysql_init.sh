c03cfed62a8ea6e4131345a1be8596beebad3151
Ketor <d.ketor@gmail.com>
2023-05-08 00:16:34 +0800

    [fix][deploy] Optimize prepare wait time of mysql init.
    Signed-off-by: Ketor <d.ketor@gmail.com>

--- scripts/mysql_init.sh
+++ scripts/mysql_init.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-sleep 60
+cd /opt/dingo-store/build/bin/ || exit 1
+DINGODB_HAVE_STORE_AVAILABLE=0
 
-# 运行 Java 程序
-java -cp /opt/dingo-store/build/bin/dingo-mysql-init-0.6.0-SNAPSHOT.jar io.dingodb.mysql.MysqlInit $SERVER_HOST:$COORDINATOR_SERVER_START_PORT
+while [ "${DINGODB_HAVE_STORE_AVAILABLE}" -eq 0 ]; do
+    echo "DINGODB_HAVE_STORE_AVAILABLE = 0, wait 1 second"
+    sleep 1
+    DINGODB_HAVE_STORE_AVAILABLE=$(./dingodb_client_coordinator --method=GetStoreMap 2>&1 >/dev/null |grep -c DINGODB_HAVE_STORE_AVAILABLE)
+done
 
-# 检查退出状态
+echo "DINGODB_HAVE_STORE_AVAILABLE = 1, start to initialize MySQL"
+cd - || exit 1
+
+# Run Java mysql initialize
+java -cp /opt/dingo-store/build/bin/dingo-mysql-init-0.6.0-SNAPSHOT.jar io.dingodb.mysql.MysqlInit "${SERVER_HOST}:${COORDINATOR_SERVER_START_PORT}"
+
+# Check if Java mysql initialize success
 if [ $? -eq 0 ]
 then
-  echo "Java mysql 初始化成功"
+  echo "Java MySQL initialize success"
 else
-  echo "Java mysql 初始化失败"
+  echo "Java MySQL initialize failed"
 fi
