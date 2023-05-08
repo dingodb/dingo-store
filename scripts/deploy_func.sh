a241183a4e1a2988c9c900776fc9949546463f0a
gary <1625721671@qq.com>
2023-05-08 14:47:26 +0800

    [chore][deploy] Fix docker mysql_init

--- scripts/deploy_func.sh
+++ scripts/deploy_func.sh
@@ -37,8 +37,9 @@ function deploy_store() {
   fi
 
   cp ${coor_service_file} $dstpath/conf/coor_list
-
-  cp $srcpath/build/bin/dingodb_server $dstpath/bin/
+  
+  unlink $dstpath/bin/dingodb_server
+  ln -s  $srcpath/build/bin/dingodb_server $dstpath/bin/dingodb_server
   if [ "${FLAGS_replace_conf}" == "0" ]; then
     cp $srcpath/conf/${role}.template.yaml $dstpath/conf/${role}.yaml
 
