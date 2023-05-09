da1a4e2cd50dfc1a2589bd7b71b0852238cb8d95
guojn1 <gjn16637211661@163.com>
2023-05-09 11:29:28 +0800

    [feat][dingo-mysql-init] Expanding global variables

--- java/dingo-mysql-init/src/main/java/io/dingodb/mysql/MysqlInit.java
+++ java/dingo-mysql-init/src/main/java/io/dingodb/mysql/MysqlInit.java
@@ -202,6 +202,10 @@ public class MysqlInit {
         values.add(new Object[]{"character_set_client", "utf8"});
         values.add(new Object[]{"character_set_connection", "utf8"});
         values.add(new Object[]{"auto_increment_increment", "1"});
+        values.add(new Object[]{"auto_increment_offset", "1"});
+        values.add(new Object[]{"protocol_version", "10"});
+        values.add(new Object[]{"port", "3307"});
+        values.add(new Object[]{"default_storage_engine", "rocksdb"});
         return values;
     }
 
