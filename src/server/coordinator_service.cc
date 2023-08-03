bde9825f424176c42740da6f0cb3817a83330e67
Ketor <d.ketor@gmail.com>
2023-08-03 05:22:16 -0400

    [feat][coordinator] Add part_id in TableDefinition and RegionDefinition.
    Signed-off-by: Ketor <d.ketor@gmail.com>

--- src/server/coordinator_service.cc
+++ src/server/coordinator_service.cc
@@ -823,6 +823,7 @@ void CoordinatorServiceImpl::CreateRegion(google::protobuf::RpcController *contr
   uint64_t schema_id = request->schema_id();
   uint64_t table_id = request->table_id();
   uint64_t index_id = request->index_id();
+  uint64_t part_id = request->part_id();
   uint64_t split_from_region_id = request->split_from_region_id();
   uint64_t new_region_id = 0;
   pb::common::RegionType region_type = request->region_type();
@@ -831,19 +832,20 @@ void CoordinatorServiceImpl::CreateRegion(google::protobuf::RpcController *contr
   butil::Status ret = butil::Status::OK();
   if (split_from_region_id > 0) {
     ret = coordinator_control_->CreateRegionForSplit(region_name, region_type, resource_tag, range, schema_id, table_id,
-                                                     index_id, index_parameter, split_from_region_id, new_region_id,
-                                                     meta_increment);
+                                                     index_id, part_id, index_parameter, split_from_region_id,
+                                                     new_region_id, meta_increment);
   } else if (request->store_ids_size() > 0) {
     std::vector<uint64_t> store_ids;
     for (auto id : request->store_ids()) {
       store_ids.push_back(id);
     }
     ret = coordinator_control_->CreateRegion(region_name, region_type, resource_tag, replica_num, range, schema_id,
-                                             table_id, index_id, index_parameter, store_ids, 0, new_region_id,
+                                             table_id, index_id, part_id, index_parameter, store_ids, 0, new_region_id,
                                              meta_increment);
   } else {
-    ret = coordinator_control_->CreateRegion(region_name, region_type, resource_tag, replica_num, range, schema_id,
-                                             table_id, index_id, index_parameter, new_region_id, meta_increment);
+    ret =
+        coordinator_control_->CreateRegion(region_name, region_type, resource_tag, replica_num, range, schema_id,
+                                           table_id, index_id, part_id, index_parameter, new_region_id, meta_increment);
   }
 
   if (!ret.ok()) {
