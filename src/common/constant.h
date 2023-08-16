b47c2b57641dd26ffdb182cff129d938202dc812
Zihui Deng <451809218@qq.com>
2023-08-16 20:17:43 +0800

    [feat][store] Adjust split_check_approximate_size depend on region_max_size.

--- src/common/constant.h
+++ src/common/constant.h
@@ -120,9 +120,9 @@ class Constant {
   static const uint32_t kDefaultSplitCheckIntervalS = 60;
   static const uint32_t kDefaultSplitCheckConcurrency = 5;
   inline static const std::string kDefaultSplitPolicy = "HALF";
-  static const uint32_t kDefaultRegionMaxSize = 134217728;              // 128M
-  static const uint32_t kDefaultSplitCheckApproximateSize = 134217728;  // 128M
-  static const uint32_t kDefaultSplitChunkSize = 1048576;               // 1M
+  static const uint32_t kDefaultRegionMaxSize = 134217728;  // 128M
+  static constexpr float kDefaultSplitCheckApproximateSizeRatio = 0.8;
+  static const uint32_t kDefaultSplitChunkSize = 1048576;  // 1M
   static constexpr float kDefaultSplitRatio = 0.5;
   static const uint32_t kDefaultSplitKeysNumber = 200000;
   static constexpr float kDefaultSplitKeysRatio = 0.5;
