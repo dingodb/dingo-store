b8039065c541296df4bc699ee87c0ca224116682
wchuande <wangchuande@outlook.com>
2024-01-02 16:18:47 +0800

    [feat][sdk] Implement Timer and ThreadPoolExecutor

---
+++ src/sdk/utils/thread_pool_executor.cc
@@ -0,0 +1,146 @@
+// Copyright (c) 2023 dingodb.com, Inc. All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "sdk/utils/thread_pool_executor.h"
+
+#include <bits/chrono.h>
+
+#include <chrono>
+#include <condition_variable>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <mutex>
+
+#include "glog/logging.h"
+#include "sdk/client.h"
+#include "sdk/utils/executor.h"
+
+namespace dingodb {
+namespace sdk {
+using namespace std::chrono;
+
+Timer::Timer() : thread_(nullptr), running_(false){};
+
+Timer::~Timer() { Stop(); }
+
+bool Timer::Start(Executor* executor) {
+  std::lock_guard<std::mutex> lk(mutex_);
+  if (running_) {
+    return false;
+  }
+
+  executor_ = executor;
+  thread_ = std::make_unique<std::thread>(&Timer::Run, this);
+  running_ = true;
+
+  return true;
+}
+
+bool Timer::Stop() {
+  {
+    std::lock_guard<std::mutex> lk(mutex_);
+    if (!running_) {
+      return false;
+    }
+
+    running_ = false;
+    while (!heap_.empty()) {
+      // TODO: add debug log
+      heap_.pop();
+    }
+
+    cv_.notify_all();
+  }
+
+  if (thread_) {
+    thread_->join();
+  }
+
+  return true;
+}
+
+bool Timer::Add(std::function<void()> func, int delay_ms) {
+  CHECK(running_);
+  auto now = steady_clock::now().time_since_epoch();
+  uint64_t next = duration_cast<microseconds>(now + milliseconds(delay_ms)).count();
+
+  FunctionInfo fn_info(std::move(func), next);
+  std::lock_guard<std::mutex> lk(mutex_);
+  heap_.push(std::move(fn_info));
+  cv_.notify_all();
+  return true;
+}
+
+void Timer::Run() {
+  std::unique_lock<std::mutex> lk(mutex_);
+  while (running_) {
+    if (heap_.empty()) {
+      cv_.wait(lk);
+      continue;
+    }
+
+    const auto& cur_fn = heap_.top();
+    uint64_t now = duration_cast<microseconds>(steady_clock::now().time_since_epoch()).count();
+    if (cur_fn.next_run_time_us <= now) {
+      std::function<void()> fn = cur_fn.fn;
+      CHECK(executor_->Execute(std::move(fn)));
+      heap_.pop();
+    } else {
+      cv_.wait_for(lk, microseconds(cur_fn.next_run_time_us - now));
+    }
+  }
+}
+
+ThreadPoolExecutor::ThreadPoolExecutor() : timer_(nullptr), pool_(nullptr), running_(false), thread_num_(0) {}
+
+ThreadPoolExecutor::~ThreadPoolExecutor() {
+  Stop();
+  timer_.reset();
+  pool_.reset();
+}
+
+bool ThreadPoolExecutor::Start(int thread_num) {
+  pool_ = std::make_unique<ThreadPool>(InternalName(), thread_num);
+  timer_ = std::make_unique<Timer>();
+  CHECK(timer_->Start(this));
+  thread_num_ = thread_num;
+  running_.store(true);
+  return true;
+}
+
+bool ThreadPoolExecutor::Stop() {
+  if (running_.load()) {
+    CHECK(timer_->Stop());
+    running_ = false;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool ThreadPoolExecutor::Execute(std::function<void()> func) {
+  CHECK(running_);
+  pool_->ExecuteTask([&](void*) { func(); }, nullptr);
+  return true;
+}
+
+bool ThreadPoolExecutor::Schedule(std::function<void()> func, int delay_ms) {
+  CHECK(running_);
+  timer_->Add(std::move(func), delay_ms);
+  return true;
+}
+
+}  // namespace sdk
+}  // namespace dingodb
---
+++ test/unit_test/sdk/test_thread_pool_executor.cc
@@ -0,0 +1,131 @@
+// Copyright (c) 2023 dingodb.com, Inc. All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+
+#include "gtest/gtest.h"
+#include "utils/thread_pool_executor.h"
+
+namespace dingodb {
+namespace sdk {
+
+static const int kThreadNum = 8;
+
+class ThreadPoolExecutorTest : public testing::Test {
+ public:
+  void SetUp() override { executor = std::make_unique<ThreadPoolExecutor>(); }
+
+  void TearDown() override { executor.reset(); }
+
+  std::unique_ptr<ThreadPoolExecutor> executor;
+};
+
+TEST_F(ThreadPoolExecutorTest, Start) {
+  bool res = executor->Start(kThreadNum);
+  EXPECT_TRUE(res);
+}
+
+TEST_F(ThreadPoolExecutorTest, Stop) {
+  bool res = executor->Start(kThreadNum);
+  EXPECT_TRUE(res);
+  res = executor->Stop();
+  EXPECT_TRUE(res);
+  res = executor->Stop();
+  EXPECT_FALSE(res);
+}
+
+TEST_F(ThreadPoolExecutorTest, ThreadNum) {
+  bool res = executor->Start(kThreadNum);
+  int num = executor->ThreadNum();
+  EXPECT_EQ(num, kThreadNum);
+}
+
+TEST_F(ThreadPoolExecutorTest, Execute) {
+  bool res = executor->Start(kThreadNum);
+  int num = executor->ThreadNum();
+  EXPECT_EQ(num, kThreadNum);
+
+  std::mutex mutex;
+  std::condition_variable cond;
+  std::atomic<int> count(2);
+
+  executor->Execute([&]() {
+    EXPECT_EQ(count.fetch_sub(1), 2);
+    cond.notify_all();
+  });
+
+  executor->Execute([&]() {
+    EXPECT_EQ(count.fetch_sub(1), 1);
+    EXPECT_EQ(count.load(), 0);
+    cond.notify_all();
+  });
+
+  int max_wait = 10;
+  {
+    std::unique_lock<std::mutex> lg(mutex);
+    while (count.load() != 0) {
+      if (max_wait-- != 0) {
+        cond.wait_for(lg, std::chrono::milliseconds(1));
+      } else {
+        break;
+      }
+    }
+  }
+
+  EXPECT_EQ(count.load(), 0);
+}
+
+TEST_F(ThreadPoolExecutorTest, Schedule) {
+  bool res = executor->Start(kThreadNum);
+
+  std::mutex mutex;
+  std::condition_variable cond;
+  std::atomic<int> count(2);
+
+  executor->Schedule(
+      [&]() {
+        EXPECT_EQ(count.fetch_sub(1), 2);
+        cond.notify_all();
+      },
+      5);
+
+  executor->Schedule(
+      [&]() {
+        EXPECT_EQ(count.fetch_sub(1), 1);
+        EXPECT_EQ(count.load(), 0);
+        cond.notify_all();
+      },
+      10);
+
+  int max_wait = 100;
+  {
+    std::unique_lock<std::mutex> lg(mutex);
+    while (count.load() != 0) {
+      if (max_wait-- != 0) {
+        cond.wait_for(lg, std::chrono::milliseconds(1));
+      } else {
+        break;
+      }
+    }
+  }
+
+  EXPECT_EQ(count.load(), 0);
+}
+
+}  // namespace sdk
+}  // namespace dingodb
