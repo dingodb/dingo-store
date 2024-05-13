98d2006d9d4ce414cab3315c1c0454b04db43e7f
wchuande <wangchuande@outlook.com>
2024-05-13 14:54:14 +0800

    [refactor][sdk] Tweak for support pydingo

--- src/pysdk/setup.py
+++ src/pysdk/setup.py
@@ -12,18 +12,18 @@ os.mkdir("dingosdk")
 shutil.copyfile("__init__.py", "dingosdk/__init__.py")
 shutil.copyfile("loader.py", "dingosdk/loader.py")
 shutil.copyfile("dingosdk.py", "dingosdk/dingosdk.py")
-ext = ".pyd" if platform.system() == 'Windows' else ".so"
+ext = ".pyd" if platform.system() == "Windows" else ".so"
 dingosdk_lib = f"_dingosdk{ext}"
 shutil.copyfile(dingosdk_lib, f"dingosdk/_dingosdk{ext}")
 
-long_description="""
+long_description = """
 dingosdk is dingo-store python sdk for efficient interact with the dingo-store cluster. 
 It contains RawKV, Txn,  Vector Api.
 NOTE: only support LINUX now.
 """
 setuptools.setup(
     name="dingosdk",
-    version="0.1rc5",
+    version="0.1rc7",
     description="dingo-store python sdk",
     license="Apache License 2.0",
     long_description=long_description,
@@ -35,11 +35,10 @@ setuptools.setup(
     author="DingoDB",
     author_email="dingodb@zetyun.com",
     keywords="dingodb dingo-store",
-
     python_requires=">=3.7",
-    packages=['dingosdk'],
+    packages=["dingosdk"],
     package_data={
-        'dingosdk': ['*.so', '*.pyd'],
+        "dingosdk": ["*.so", "*.pyd"],
     },
     zip_safe=False,
 )
