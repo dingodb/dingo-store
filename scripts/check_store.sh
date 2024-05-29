007dad0bdba356eb7cc66627314d004b3cb07c86
Zihui Deng <451809218@qq.com>
2024-05-29 11:45:45 +0800

    [chore][deploy] Add retry times limit for check script.

--- scripts/check_store.sh
+++ scripts/check_store.sh
@@ -4,6 +4,8 @@ mydir="${BASH_SOURCE%/*}"
 if [[ ! -d "$mydir" ]]; then mydir="$PWD"; fi
 . $mydir/shflags
 
+DEFINE_integer retry_times 64 'retry times'
+
 BASE_DIR=$(dirname $(cd $(dirname $0); pwd))
 DIST_DIR=$BASE_DIR/dist
 
@@ -13,25 +15,34 @@ echo ${DINGODB_BIN}
 
 cd ${DINGODB_BIN}
 
-DINGODB_HAVE_STORE_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_STORE_AVAILABLE)
-while [ "${DINGODB_HAVE_STORE_AVAILABLE}" -eq 0 ]; do
-    echo "DINGODB_HAVE_STORE_AVAILABLE == 0, wait 2 second"
-    sleep 2
+times=0
+DINGODB_HAVE_STORE_AVAILABLE=0
+while [ "${DINGODB_HAVE_STORE_AVAILABLE}" -eq 0 -a ${times} -lt ${FLAGS_retry_times} ]; do
     DINGODB_HAVE_STORE_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_STORE_AVAILABLE)
+    times=`expr $times + 1`
+
+    echo "avaiable store count = ${DINGODB_HAVE_STORE_AVAILABLE}, times = ${times}, wait 2 second"
+    sleep 2
 done
 
-DINGODB_HAVE_INDEX_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_INDEX_AVAILABLE)
-while [ "${DINGODB_HAVE_INDEX_AVAILABLE}" -eq 0 ]; do
-    echo "DINGODB_HAVE_INDEX_AVAILABLE == 0, wait 2 second"
-    sleep 2
+times=0
+DINGODB_HAVE_INDEX_AVAILABLE=0
+while [ "${DINGODB_HAVE_INDEX_AVAILABLE}" -eq 0 -a ${times} -lt ${FLAGS_retry_times} ]; do
     DINGODB_HAVE_INDEX_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_INDEX_AVAILABLE)
+    times=`expr $times + 1`
+
+    echo "avaiable index count = ${DINGODB_HAVE_INDEX_AVAILABLE}, times = ${times}, wait 2 second"
+    sleep 2
 done
 
-DINGODB_HAVE_DOCUMENT_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_DOCUMENT_AVAILABLE)
-while [ "${DINGODB_HAVE_DOCUMENT_AVAILABLE}" -eq 0 ]; do
-    echo "DINGODB_HAVE_DOCUMENT_AVAILABLE == 0, wait 2 second"
-    sleep 2
+times=0
+DINGODB_HAVE_DOCUMENT_AVAILABLE=0
+while [ "${DINGODB_HAVE_DOCUMENT_AVAILABLE}" -eq 0 -a ${times} -lt ${FLAGS_retry_times} ]; do
     DINGODB_HAVE_DOCUMENT_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_DOCUMENT_AVAILABLE)
+    times=`expr $times + 1`
+
+    echo "avaiable document count = ${DINGODB_HAVE_DOCUMENT_AVAILABLE}, times = ${times}, wait 2 second"
+    sleep 2
 done
 
 ./dingodb_client GetStoreMap
