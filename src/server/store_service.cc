d266e3ee53f9d9310bb9cbba91481754dc38c522
yangjundong <1047934838@qq.com>
2024-07-12 18:29:55 +0000

    [test][store] Add pessimistic lock unit test

--- src/server/store_service.cc
+++ src/server/store_service.cc
@@ -1983,7 +1983,7 @@ void DoTxnPessimisticLock(StoragePtr storage, google::protobuf::RpcController* c
   }
 
   std::vector<pb::common::KeyValue> kvs;
-  // yjddebug todo
+
   status = storage->TxnPessimisticLock(ctx, mutations, request->primary_lock(), request->start_ts(),
                                        request->lock_ttl(), request->for_update_ts(), true, kvs);
   if (!status.ok()) {
