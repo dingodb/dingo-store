44ec74b109ece59178978c34f337101656fb2b3b
yangjundong <1047934838@qq.com>
2025-01-22 08:49:07 +0000

    [chore][deploy] Replace dingo_client with dingo_cli

--- scripts/check_store.sh
+++ scripts/check_store.sh
@@ -18,7 +18,7 @@ cd ${DINGODB_BIN}
 times=0
 DINGODB_HAVE_STORE_AVAILABLE=0
 while [ "${DINGODB_HAVE_STORE_AVAILABLE}" -eq 0 -a ${times} -lt ${FLAGS_retry_times} ]; do
-    DINGODB_HAVE_STORE_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_STORE_AVAILABLE)
+    DINGODB_HAVE_STORE_AVAILABLE=$(./dingodb_cli GetStoreMap |grep -c DINGODB_HAVE_STORE_AVAILABLE)
     times=`expr $times + 1`
 
     echo "avaiable store count = ${DINGODB_HAVE_STORE_AVAILABLE}, times = ${times}, wait 2 second"
@@ -28,7 +28,7 @@ done
 times=0
 DINGODB_HAVE_INDEX_AVAILABLE=0
 while [ "${DINGODB_HAVE_INDEX_AVAILABLE}" -eq 0 -a ${times} -lt ${FLAGS_retry_times} ]; do
-    DINGODB_HAVE_INDEX_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_INDEX_AVAILABLE)
+    DINGODB_HAVE_INDEX_AVAILABLE=$(./dingodb_cli GetStoreMap |grep -c DINGODB_HAVE_INDEX_AVAILABLE)
     times=`expr $times + 1`
 
     echo "avaiable index count = ${DINGODB_HAVE_INDEX_AVAILABLE}, times = ${times}, wait 2 second"
@@ -38,14 +38,14 @@ done
 times=0
 DINGODB_HAVE_DOCUMENT_AVAILABLE=0
 while [ "${DINGODB_HAVE_DOCUMENT_AVAILABLE}" -eq 0 -a ${times} -lt ${FLAGS_retry_times} ]; do
-    DINGODB_HAVE_DOCUMENT_AVAILABLE=$(./dingodb_client GetStoreMap |grep -c DINGODB_HAVE_DOCUMENT_AVAILABLE)
+    DINGODB_HAVE_DOCUMENT_AVAILABLE=$(./dingodb_cli GetStoreMap |grep -c DINGODB_HAVE_DOCUMENT_AVAILABLE)
     times=`expr $times + 1`
 
     echo "avaiable document count = ${DINGODB_HAVE_DOCUMENT_AVAILABLE}, times = ${times}, wait 2 second"
     sleep 2
 done
 
-./dingodb_client GetStoreMap
+./dingodb_cli GetStoreMap
 
 echo "dingo-store is READY"
 
