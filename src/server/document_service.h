f82465fbd483ce152e6448915da47527bb199452
yangjundong <1047934838@qq.com>
2025-03-19 09:20:38 +0000

    [fix][store] remove useless code

--- src/server/document_service.h
+++ src/server/document_service.h
@@ -103,9 +103,6 @@ class DocumentServiceImpl : public pb::document::DocumentService {
   void BackupData(google::protobuf::RpcController* controller, const dingodb::pb::store::BackupDataRequest* request,
                   dingodb::pb::store::BackupDataResponse* response, google::protobuf::Closure* done) override;
 
-  void RestoreMeta(google::protobuf::RpcController* controller, const dingodb::pb::store::RestoreMetaRequest* request,
-                   dingodb::pb::store::RestoreMetaResponse* response, google::protobuf::Closure* done) override;
-
   void RestoreData(google::protobuf::RpcController* controller, const dingodb::pb::store::RestoreDataRequest* request,
                    dingodb::pb::store::RestoreDataResponse* response, google::protobuf::Closure* done) override;
 
